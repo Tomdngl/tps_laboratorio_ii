91d6bcc94f01939cb4c838ce7426d30754341ec1
Tomas D'Angelo <tomidngl@gmail.com>
2021-09-16 23:13:33 -0300

    Avance tp1

---
+++ TP1/Entidades/Operando.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+        private string Numero
+        {
+            set
+            {
+                this.numero = ValidarOperando(value);
+            }
+        }
+
+        public static string BinarioDecimal(string binario)
+        {
+            string result = "Valor inválido";
+
+            if(EsBinario(binario))
+            {
+                int numero = 0;
+                int digito;
+
+                for (long i = int.Parse(binario), j = 0; i > 0; i /= 10, j++)
+                {
+                    digito = (int)i % 10;
+                    numero += digito * (int)Math.Pow(2, j);
+                }
+
+                result = numero.ToString();
+            }
+
+            return result;
+        }
+
+        private static bool EsBinario(string binario)
+        {
+            bool isOk = true;
+            foreach (char c in binario)
+            {
+                if(c != '1' && c != '0')
+                {
+                    isOk = false;
+                }
+            }
+            return isOk;
+        }
+        public Operando()
+        {
+            this.numero = 0;
+        }
+
+        public Operando(double numero) : this()
+        {
+            this.numero = numero;
+        }
+
+        public Operando(string strNumero) : this()
+        {
+            Numero = strNumero;
+        }
+
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+        public static double operator /(Operando n1, Operando n2)
+        {
+            return n1.numero / n2.numero;
+        }
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+        private double ValidarOperando(string numero)
+        {
+            return Convert.ToDouble(numero);
+        }
+
+    }
+}
---
+++ TP1/TestConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Entidades;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Operando.BinarioDecimal("101"));
+            Console.ReadLine();
+        }
+    }
+}
